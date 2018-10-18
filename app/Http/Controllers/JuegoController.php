c5e7d95bca478c1604a5605234d064f48317f999
Excarline <pedrozj11@hotmail.com>
2018-10-18 17:51:00 +0200

    Mas del controller hecho

--- app/Http/Controllers/JuegoController.php
+++ app/Http/Controllers/JuegoController.php
@@ -9,58 +9,180 @@ class JuegoController extends Controller
    public function preguntar(Request $request){
        
         $query="PARTE COMUN";
+        $respuestas= explode(",",$request->input('almacenamiento'));
+        
+        array_push($respuestas, $request->input('respuesta'));
 
-       $respuestas= explode(",",$request->input('almacenamiento'));
-       
 
-
-       array_push($respuestas, $request->input('respuesta'));
-
+        /* PRIMERAS 5 PREGUNTAS*/
 
        if(sizeof($respuestas)==5){
 
 
-            if(respuestas[0]==1){
+            if($i==0){
 
-                $query.="";
-            
-            }
-            
-            if(respuestas[1]==1){
+                if(respuestas[0]==1){
 
-                $query.="";
-            
-            }
+                    $query.="";
+                   
+                }
 
-            
-            if(respuestas[2]==1){
+                else{
 
-                $query.="";
-            
-            }
+                    $query.="";
 
-            
-            if(respuestas[3]==1){
-
-                $query.="";
+                }
+               
             
             }
 
-            
-            if(respuestas[4]==1){
+            if($i==1){
 
-                $query.="";
+                if(respuestas[1]==1){
+
+                    $query.="";
+                   
+                }
+
+                else{
+
+                    $query.="";
+
+                }
+               
             
             }
 
+            if($i==2){
+
+                if(respuestas[2]==1){
+
+                    $query.="";
+                   
+                }
+
+                else{
+
+                    $query.="";
+
+                }
+               
+            
+            }
+            
+
+            if($i==3){
+
+                if(respuestas[3]==1){
+
+                    $query.="";
+                }
+
+                else{
+
+                    $query.="";
+
+                }
+               
+            
+            }
+            
+
+            if($i==4){
+
+                if(respuestas[4]==1){
+
+                    $query.="";
+                   
+                }
+
+                else{
+
+                    $query.="";
+
+                }
+               
+            
+            }
+            
+                    
         /* HACER QUERY  SPARQL*/ 
 
-        if(COUNT($respuesta)==1)
+        if(COUNT($respuesta)==1){
+
+            return view('finJuego', ['bool'=>'ganaste']);
+
+        }
 
        }
 
+    /*SIGUIENTES 3 PREGUNTAS */
+
+
+    if(sizeof($respuestas)==8){
+
+        for ($i=5; $i < 7; $i++) {
+            
+            if($i==5){
+
+                if(respuestas[5]==1){
+
+                    $query.="";
+                
+                }
+
+                else{
+
+                    $query.="";
+
+                }
+            
+            
+            }
+
+            if($i==6){
+
+                if(respuestas[6]==1){
+
+                    $query.="";
+                
+                }
+
+                else{
+
+                    $query.="";
+
+                }
+            
+            
+            }
+
+            if($i==7){
+
+                if(respuestas[7]==1){
+
+                    $query.="";
+                
+                }
+
+                else{
+
+                    $query.="";
+
+                }
+            
+            
+            }
+        }
+        if(COUNT($respuesta)==1){
+
+            return view('finJuego', ['bool'=>'ganaste']);
+
+        }
+        
        
-       
+    }
+           
         
        return view('juego', ['respuestas' => $respuestas]);
 

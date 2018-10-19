13b7e728dcd25720f1acbb459b16f7c51a78a8a0
Excarline <pedrozj11@hotmail.com>
2018-10-19 13:48:07 +0200

    CSS en otras vistas aplicado

--- resources/views/finJuego.blade.php
+++ resources/views/finJuego.blade.php
@@ -9,14 +9,12 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
             <link rel="stylesheet" type="text/css" href="css/design.css">
 </head>
-<body>
+<body class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
     
-
-        Se ha terminado el juego. Gané!
-
-        <br>
+    <h1 style="margin-bottom: 60px;"> Se ha terminado el juego. Gané!</h1 >
+       
         
-        <a href="/juego">Volver a jugar</a>
+        <a href="/juego" class="btn btn-primary" style="font-size:22px;">Volver a jugar</a>
 
 </body>
 </html>
--- resources/views/finJuegoMalo.blade.php
+++ resources/views/finJuegoMalo.blade.php
@@ -1,8 +1,21 @@
-Shit! He perdido.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Perdiste</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+            <link rel="stylesheet" type="text/css" href="css/design.css">
+</head>
+<body class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+    
+    <h1 style="margin-bottom: 60px;"> Shit! He perdido. Ganaste!</h1 >
+       
+        
+        <a href="/juego" class="btn btn-primary" style="font-size:22px;">Volver a jugar</a>
 
-<br>
-Ganaste!
+</body>
+</html>
 
-<br>
-
-<a href="/juego">Volver a jugar</a>
